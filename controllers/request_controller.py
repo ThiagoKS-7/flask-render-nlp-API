1d97b2ffcec04d6b4c47337afb9864a97addd7d9
Thiago Kasper de Souza <thiago.souza@meta.com.br>
2023-02-28 01:13:46 -0300

    #1-release: implemenmtando get-users

--- controllers/request_controller.py
+++ controllers/request_controller.py
@@ -3,6 +3,10 @@ from flask import Response
 from handlers.register_handler import RegisterHandler
 from handlers.detect_handler import DetectHandler
 
+handlers = {
+  "register": RegisterHandler,
+  "detect": DetectHandler,
+}
 
 def checkPostedData(collection,body, functionName):
   if  all( s not in body for s in ['username', 'password']):
@@ -10,18 +14,13 @@ def checkPostedData(collection,body, functionName):
       "message": "Error: missing required parameter usr/pwd.",
       "status": 401
     }
-  res = {
-    "register": RegisterHandler(collection, body),
-    "detect": DetectHandler(collection, body),
-  }
-  if functionName in res:
-    return res[functionName]
+
+  if functionName in handlers:
+    return handlers[functionName].start(collection, body)
   
 
 def handleRequest(collection,body, routeName):
     try:  
-      res = checkPostedData(collection,body, routeName)
-      return res
+      return  checkPostedData(collection,body, routeName)
     except Exception as e:
-      error = Response("Error!  " + str(e),status=400)
-      return error
+      return Response("Error!  " + str(e),status=400)
--- handlers/users_handler.py
+++ handlers/users_handler.py
@@ -1,40 +1,37 @@
-#   elif functionName == 'get-users':
-#     usr = body["username"]
-#     pwd_exists = collection.find_one({'Username': usr})["Password"]
-#     # se existe um password pra aquele user, é pq ele existe
-#     if pwd_exists:
-#       #faz update
-#       tokens = collection.find_one({'Username': usr})["Tokens"]
-#       # se o user tiver tokens, deixa passar
-#       if tokens <= 0:
-#         retJson = {
-#           "message":"Error! Not enough tokens.",
-#           "status": 401
-#         }
-#       else:
-#         tokens = tokens -1
-#         result = []
-#         for col in collection.find({},{"Username":1, "Text1":1, "Text2":1, "Tokens":1}):
-#           result.append({
-#             "username":col["Username"],
-#             "text1":col["Text1"],
-#             "text2":col["Text2"],
-#             "tokens":col["Tokens"],
-#           })
-        
-#         retJson = {
-#           "message":"Data successfully retrieved.",
-#           "data": result,
-#           "Tokens": tokens,
-#           "status": 200,
-#         }
-#     else:
-#       retJson = {
-#       "message":"Error! User or password incorrect.",
-#       "status": 400
-#       }
 
-#     return retJson
+
+class UsersHandler:
+    def __init__(self):
+        pass
+    def start(collection:any, body:any) -> any:
+        usr = body["username"]
+        
+        if collection.find_one({'Username': usr})["Password"]:
+            tokens = collection.find_one({'Username': usr})["Tokens"]
+            if tokens <= 0:
+                return {
+                    "message":"Error! Not enough tokens.",
+                    "status": 401
+                }
+            tokens -= 1
+            result = [{
+                        "username":col["Username"],
+                        "text1":col["Text1"],
+                        "text2":col["Text2"],
+                        "tokens":col["Tokens"],
+                    } for col in collection.find({},{"Username":1, "Text1":1, "Text2":1, "Tokens":1})]
+                    
+            return {
+                "message":"Data successfully retrieved.",
+                "data": result,
+                "Tokens": tokens,
+                "status": 200,
+            }
+                
+        return {
+            "message":"Error! User or password incorrect.",
+            "status": 400
+            }
 
 #   elif functionName == 'refill':
 #     usr = body["username"]
