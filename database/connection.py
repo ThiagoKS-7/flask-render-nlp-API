59b93d7f50e61c19359ca22048dc1bfc7ba12e3f
Thiago Kasper de Souza <thiago.souza@meta.com.br>
2023-03-03 11:44:11 -0300

    #2-release: env e connection

--- database/connection.py
+++ database/connection.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from decouple import config
 
-path = f"mongodb+srv://{(config('DB_USR'))}:{config('DB_PASSWORD')}@cluster0.8nqzucw.mongodb.net/?retryWrites=true&w=majority"
+path = f"mongodb+srv://{(config('DB_USR'))}:{config('DB_PASSWORD')}@{config('DB_NAME')}.{config('DB_CLUSTER')}.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(path)
 def db():
     return client.SimiliarityDatabase
