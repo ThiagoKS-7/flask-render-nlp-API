475d34b6f14780cb16b326529e22b317b681f7e2
Thiago Kasper de Souza <thiago.souza@meta.com.br>
2023-03-03 11:00:35 -0300

    #2-release: ajustando dao, banco e template

--- database/connection.py
+++ database/connection.py
@@ -2,8 +2,7 @@ from pymongo import MongoClient
 from decouple import config
 
 path = f"mongodb+srv://{(config('DB_USR'))}:{config('DB_PASSWORD')}@cluster0.8nqzucw.mongodb.net/?retryWrites=true&w=majority"
-print(path)
-client = MongoClient(path,27017)
+client = MongoClient(path)
 def db():
     return client.SimiliarityDatabase
 
--- repositories/users_dao.py
+++ repositories/users_dao.py
@@ -9,12 +9,6 @@ class UsersDao:
         
         if self.collection.find_one({'Username': usr})["Password"]:
             tokens = self.collection.find_one({'Username': usr})["Tokens"]
-            if tokens <= 0:
-                return {
-                    "message":"Error! Not enough tokens.",
-                    "status": 401
-                }
-            tokens -= 1
             result = [{
                         "username":col["Username"],
                         "text1":col["Text1"],
@@ -30,32 +24,6 @@ class UsersDao:
             }
                 
         return {
-            "message":"Error! User or password incorrect.",
+            "message":"Error! Wrong User or password",
             "status": 400
             }
-
-#   elif functionName == 'refill':
-#     usr = body["username"]
-#     target=body["target"]
-#     pwd = body["password"]
-#     tokens = body["tokens"]
-#     pwd_exists = collection.find_one({'Username': usr})["Password"]
-#     #se existe um password pra aquele user, é pq ele existe
-#     if pwd_exists and usr == 'admin':
-#       #faz update
-#       collection.update_one({'Username': target}, {"$set": {
-#         "Tokens": tokens,
-#         "UpdatedAt": datetime.now(),
-#       }})
-#       retJson = {
-#         "message":"Sentence successfully saved.",
-#         "Tokens": tokens,
-#         "status": 200,
-#       }
-#     else:
-#       retJson = {
-#       "message":"Error! Only admin can refill tokens.",
-#       "status": 400
-#       }
-
-#     return retJson
